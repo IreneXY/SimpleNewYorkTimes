fb519116074953831f6c3cbf5beb1b4d6f0ef5fe
IreneXY <irene.xin.yu@gmail.com>
2017-09-20 08:18:53 -0700

    ini project

---
+++ android/app/src/main/java/com/mintminter/simplenewyorktimes/api/Bootstrap.java
@@ -0,0 +1,11 @@
+package com.mintminter.simplenewyorktimes.api;
+
+import com.mintminter.simplenewyorktimes.BuildConfig;
+
+/**
+ * Created by Irene on 9/20/17.
+ */
+
+public class Bootstrap {
+    public static final String NYTAPIKEY = BuildConfig.NYTAPIKEY;
+}
