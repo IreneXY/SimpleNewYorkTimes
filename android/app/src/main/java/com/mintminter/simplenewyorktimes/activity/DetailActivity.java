afc723cb25fdb8291acd64b25560e628d720dd3b
IreneXY <irene.xin.yu@gmail.com>
2017-09-24 17:25:23 -0700

    can share article title and url to friends using apps like messages, email, Facebook, Bluetooth, etc.

--- android/app/src/main/java/com/mintminter/simplenewyorktimes/activity/DetailActivity.java
+++ android/app/src/main/java/com/mintminter/simplenewyorktimes/activity/DetailActivity.java
@@ -1,10 +1,15 @@
 package com.mintminter.simplenewyorktimes.activity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,6 +27,9 @@ public class DetailActivity extends AppCompatActivity {
     private RelativeLayout mProgressBar;
     private WebView mWebView;
 
+    private ShareActionProvider mShareAction;
+    private Intent mShareIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,17 +39,13 @@ public class DetailActivity extends AppCompatActivity {
         if(TextUtils.isEmpty(title) || TextUtils.isEmpty(url)){
             finish();
         }
+        prepareShareIntent(title, url);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
-        toolbar.setNavigationIcon(R.mipmap.ic_back);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        toolbar.setTitle(title);
-        //setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         mProgressBar = (RelativeLayout) findViewById(R.id.detail_progressbar);
 
@@ -65,4 +69,34 @@ public class DetailActivity extends AppCompatActivity {
         mWebView.loadUrl(url);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        return true;
+
+    }
+
+    private void prepareShareIntent(String title, String text) {
+        mShareIntent = new Intent(Intent.ACTION_SEND);
+        mShareIntent.setType("text/plain");
+        mShareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,title);
+        mShareIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.detail_share:
+                startActivity(Intent.createChooser(mShareIntent, "Shearing Option"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
